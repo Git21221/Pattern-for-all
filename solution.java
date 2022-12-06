cf61803f6712c258b290b861bc67b4e6ceb5d19c
Git21221 <101005577+Git21221@users.noreply.github.com>
2022-12-06 11:48:47 +0530

    Add files via upload

--- solution.java
+++ solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    static void printSquare(int n) {
+    private static void printTriangle(int n) {
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j <= i; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args){
-        printSquare(5);
+        printTriangle(5);
     }
 }
